908cd8461be9ae80a3c33809d62bc19604a44ee1
k2eunwoo <103038209+k2eunwoo@users.noreply.github.com>
2022-05-10 00:09:22 +0900

    kew 0510

--- js/main.js
+++ js/main.js
@@ -8,6 +8,22 @@ $(function(){
     autoplay:true,
     autoplaySpeed : 500,
   });
+  var swiper = new Swiper(".mySwiper", {
+    loop:false,
+    slidesPerView: 3.4,
+    arrows: true,
+    autoplay:false,
+    spaceBetween: 50,
+    delay: 5000, // 시간 설정
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    scrollbar: {
+        el: ".swiper-scrollbar",
+    },
+  });
+
   $('.slider-nav').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -49,6 +65,32 @@ $(function(){
       }
     ]
   });
+
+   $(function() {
+       //input을 datepicker로 선언
+       $("#datepicker").datepicker({
+           dateFormat: 'yy-mm-dd' //달력 날짜 형태
+           ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+           ,showMonthAfterYear:true // 월- 년 순서가아닌 년도 - 월 순서
+           ,changeYear: true //option값 년 선택 가능
+           ,changeMonth: true //option값  월 선택 가능                
+           ,showOn: "both" //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시  
+           ,buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" //버튼 이미지 경로
+           ,buttonImageOnly: true //버튼 이미지만 깔끔하게 보이게함
+           ,buttonText: "선택" //버튼 호버 텍스트              
+           ,yearSuffix: "년" //달력의 년도 부분 뒤 텍스트
+           ,monthNamesShort: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 텍스트
+           ,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip
+           ,dayNamesMin: ['일','월','화','수','목','금','토'] //달력의 요일 텍스트
+           ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 Tooltip
+           ,minDate: "-5Y" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+           ,maxDate: "+5y" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)  
+       });                    
+       
+       //초기값을 오늘 날짜로 설정해줘야 합니다.
+       $('#datepicker').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)            
+   });
+
 // 더보기 버튼
   $(function(){
     /* 1단계 설명  : for
@@ -111,66 +153,13 @@ $(".lazy3").slick({
   dots:true,
 });
 
-  var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    slidesPerView: 3.4,
-    arrows: true,
-    autoplay:false,
-    spaceBetween: 50,
-    delay: 5000, // 시간 설정
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    scrollbar: {
-        el: ".swiper-scrollbar",
-    },
-  });
-  // 네이버지도
-  var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
-};
-var map = new naver.maps.Map('map', {
-  center: new naver.maps.LatLng(37.3595704, 127.105399),
-  zoom: 10
-});
-  var map = null;
-function initMap() {
-    map = new naver.maps.Map('map', {
-        center: new naver.maps.LatLng(37.3595704, 127.105399),
-        zoom: 10
-    });
-}
-
-var map = new naver.maps.Map('map', mapOptions);
-
-$(document).ready(function(){
-  $('section[data-type="background"]').each(function(){
-      var $bgobj = $(this); // assigning the object
-  
-      $(window).scroll(function() {
-          var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-          
-          // Put together our final background position
-          var coords = '50% '+ yPos + 'px';
-
-          // Move the background
-          $bgobj.css({ backgroundPosition: coords });
-      }); 
-  });    
-});
-$(document).ready(function() {
-  var wd = $(window);
-  $('.paral').each(function(){
-    var bg = $(this);
-    wd.scroll(function(){
-      var yPos = -(bg.scrollTop() / 2); 
-        //나눠지는 숫자가 작을 수록 속도가 빠름
-      var coords = '0' + yPos + 'px';
-      bg.css({backgroundPosition:coords});
-    });
-  });
-});
+// 카카오맵 지도 api
+var container = document.getElementById('map');
+		var options = {
+			center: new kakao.maps.LatLng('4W5V'+'P4', 부산광역시),
+			level: 3
+		};
+		var map = new kakao.maps.Map(container, options);
+ 
 })//ready
 
